d503dc6a8465bf7bbabbea375ac9787a7bc279e5
Dan Murphy <dmurphy@ti.com>
2020-04-17 15:03:43 -0500

    sound: tas2562: Update license to include BSD-3
    Signed-off-by: Dan Murphy <dmurphy@ti.com>

--- sound/soc/codecs/tas2562.c
+++ sound/soc/codecs/tas2562.c
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0
+// SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause)
 //
 // Driver for the Texas Instruments TAS2562 CODEC
 // Copyright (C) 2019 Texas Instruments Inc.
